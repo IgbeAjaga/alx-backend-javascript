0411a8ef8dace3aaf75074b340bb1e7741dc1825
IgbeAjaga <ajagaigbe144@gmail.com>
2023-11-28 23:48:09 +0000

    user profile

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
     results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      status: result.status === 'fulfilled' ? 'fulfilled' : 'rejected',
+      value: result.value ? result.value : result.reason,
     }))
   );
 }
