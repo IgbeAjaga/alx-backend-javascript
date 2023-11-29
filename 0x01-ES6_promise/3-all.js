1ed8b8e75bbb233362622c3eb29cc7eb34787443
IgbeAjaga <ajagaigbe144@gmail.com>
2023-11-29 00:02:24 +0000

    import uploadPhoto and createUser from utils.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      results.forEach((result) => {
-        if (result.status === 200 && result.body) {
-          console.log(result.body.firstName, result.body.lastName);
-        } else {
-          console.log('Signup system offline');
-        }
-      });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
