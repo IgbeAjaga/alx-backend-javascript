d48be3083c8367d7be13b7480f4f5f062864f6b7
IgbeAjaga <ajagaigbe144@gmail.com>
2023-11-29 00:16:12 +0000

    Write and export a function named handleProfileSignup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => (
-      result.map(o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
+    results.map((result) => ({
+      status: result.status === 'fulfilled' ? 'fulfilled' : 'rejected',
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))
+  );
 }
