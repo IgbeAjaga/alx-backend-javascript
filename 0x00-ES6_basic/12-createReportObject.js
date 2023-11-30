699aed125efa51f35e2e38fae8f28f5692d8672c
IgbeAjaga <ajagaigbe144@gmail.com>
2023-11-30 02:14:20 +0000

    code fixed

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = { ...employeesList };
-
-  const getNumberOfDepartments = (employees) => {
-    return Object.keys(employees).length;
-  };
-
   return {
-    allEmployees,
-    getNumberOfDepartments: () => getNumberOfDepartments(allEmployees),
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
 }
