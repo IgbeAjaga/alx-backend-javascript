d594694109ee07efe7b521ede48a247719c06b28
IgbeAjaga <ajagaigbe144@gmail.com>
2023-11-30 02:17:48 +0000

    code fixed

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,7 +2,7 @@ export default function iterateThroughObject(reportWithIterator) {
   const employees = [];
 
   for (const item of reportWithIterator) {
-    employees.push(item.value);
+    employees.push(item);
   }
 
   return employees.join(' | ');
