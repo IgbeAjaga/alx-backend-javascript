e4831d38fb342280c59ce8d67ae4abb48fd57b29
IgbeAjaga <ajagaigbe144@gmail.com>
2023-11-30 11:47:32 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,25 @@
+const CarSymbol = Symbol('CarSymbol');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const clonedCar = new Car();
+    Object.getOwnPropertyNames(this).forEach(prop => {
+      clonedCar[prop] = this[prop];
+    });
+    return clonedCar;
+  }
+
+  [CarSymbol]() {
+    return {
+      brand: this._brand,
+      motor: this._motor,
+      color: this._color,
+    };
+  }
+}
