9ad28925b362a5bf15cda65d3c7c0f72ae47948a
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-01 00:44:10 +0000

    eslint update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -38,6 +32,12 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
+    if (!(newStudents instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!newStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = newStudents;
   }
 }
