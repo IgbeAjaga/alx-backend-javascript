18f8b2556e03d157780b7f966171d1a44caafb81
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-01 01:18:24 +0000

    eslint update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    const resultArray = [];
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName)
-    ]).then((results) => {
-        results.forEach((result) => {
-            resultArray.push({
-                status: result.status,
-                value: result.status === 'rejected' ? result.reason.toString() : result.value
-            });
-        });
-        return resultArray;
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
