b33c5b092acfafc61c531ebab743f149c19b435b
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-05 23:15:26 +0000

    get record

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * Create a function named setFromArray that returns a Set from an array
+ * @param {Array} array - The source array.
+ * @author Igbe Ajaga
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+	// returns a set of array
+  return new Set(array);
+}
