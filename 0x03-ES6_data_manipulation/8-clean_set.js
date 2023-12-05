73fea6a9687fcfb4833db1755e226a6a09d329af
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-05 23:22:25 +0000

    get record

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,26 @@
+/**
+ * a function named cleanSet that returns a string of all the set
+ * values that start with a specific string (startString).
+ * It accepts two arguments: a set (Set) and a startString (String). 
+ * @param {Set<String>} set - A collection of strings.
+ * @param {String} startString - The string to strip from the beginning
+ * of each item in the set.
+ * @author Igbe Ajaga
+ * @returns {String}
+ */
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
+}
