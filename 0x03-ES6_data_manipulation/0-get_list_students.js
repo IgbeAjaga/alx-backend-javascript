83abcb3afc19498cd976901acadcb2f8164ec07c
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-05 21:50:13 +0000

    function named getListStudents that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+export default function getListStudents() {
+    // This function should retrieve a list of students.
+    // Replace this placeholder with the logic to fetch the student list.
+    // For example:
+    const studentList = [
+	    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	    { id: 2, firstName: 'James', location: 'Columbia' },
+	    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+    return studentList;
+}
