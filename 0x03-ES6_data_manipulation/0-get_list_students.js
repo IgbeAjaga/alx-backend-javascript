8298cf3f2e73b9fccd15c68a99749a66bca22b8e
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-05 21:58:04 +0000

    eslint update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,9 +3,9 @@ export default function getListStudents() {
     // Replace this placeholder with the logic to fetch the student list.
     // For example:
     const studentList = [
-	    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	    { id: 2, firstName: 'James', location: 'Columbia' },
-	    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
     return studentList;
 }
