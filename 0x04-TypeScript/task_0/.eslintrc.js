22c5780440a79491722b4655b95c37e3bbfe9d39
IgbeAjaga <ajagaigbe144@gmail.com>
2023-12-06 22:26:05 +0000

    typescript

---
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended', //@typescript eslint-plugin
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
