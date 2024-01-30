fdfc05c73d241dcd01596c927d754cfbe2e8cc58
Igbe Ajaga <ajagaigbe144@gmail.com>
2024-01-31 00:57:15 +0100

    basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT
+ * @param {String} msg The message to display.
+ * @author Igbe Ajaga <https://github.com/IgbeAjaga>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
