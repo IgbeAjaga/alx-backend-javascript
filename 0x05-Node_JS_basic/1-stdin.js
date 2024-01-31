0d3e37af99461a62088b1db2aca4bc4705e64186
Igbe Ajaga <ajagaigbe144@gmail.com>
2024-01-31 02:39:24 +0100

    basic javascript with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,14 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
+
   process.stdout.write('This important software is now closing\n');
   process.exit();
 });
