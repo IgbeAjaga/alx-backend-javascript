0bd4c22e3314c339adbc1f6c60266dea03d1ec16
IgbeAjaga <ajagaigbe144@gmail.com>
2024-02-01 10:28:02 -0800

    js unittest

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,33 +1,29 @@
 const sinon = require('sinon');
-const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./5-payment');
 const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment'); // Updated to reflect the filename change
 
 describe('sendPaymentRequestToApi', () => {
   let calculateNumberStub;
-  let consoleSpy;
+  let consoleLogSpy;
 
   beforeEach(() => {
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
-    consoleSpy = sinon.spy(console, 'log');
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
   });
 
   afterEach(() => {
     calculateNumberStub.restore();
-    consoleSpy.restore();
+    consoleLogSpy.restore();
   });
 
-  it('should log correct message and only be called once', () => {
-    calculateNumberStub.returns(120);
+  it('should log the correct total for 100 and 20', () => {
     sendPaymentRequestToApi(100, 20);
-
-    expect(consoleSpy.calledOnceWith('The total is: 120')).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
   });
 
-  it('should log correct message and only be called once', () => {
-    calculateNumberStub.returns(20);
+  it('should log the correct total for 10 and 10', () => {
     sendPaymentRequestToApi(10, 10);
-
-    expect(consoleSpy.calledOnceWith('The total is: 20')).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
   });
 });
