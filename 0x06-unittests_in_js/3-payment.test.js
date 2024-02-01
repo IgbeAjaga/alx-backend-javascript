8293f4840dc3e986042c717d8277a9d02193a934
IgbeAjaga <ajagaigbe144@gmail.com>
2024-02-01 07:51:49 -0800

    JS unittest

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,17 +1,15 @@
 const sinon = require('sinon');
+const Utils = require('./utils');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
-const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with correct arguments', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
-    calculateNumberStub.returns(120);
-    
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const calculateNumberStub = sinon.spy(Utils);
+
     sendPaymentRequestToApi(100, 20);
-    
-    expect(calculateNumberStub.calledWith(100, 20)).to.be.true;
-    
-    calculateNumberStub.restore();
+    expect(calculateNumberStub.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberStub.calculateNumber.callCount).to.be.equal(1);
+    calculateNumberStub.calculateNumber.restore();
   });
 });
